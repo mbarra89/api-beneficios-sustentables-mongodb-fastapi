d446294cadc38eba4fce5f67b6005d5c2e0f3b4b
Miguel Barra <mbarra486@icloud.com>
2024-08-20 18:00:29 -0400

    Implement AWS Lambda deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 from app.api.routers.beneficios import router as beneficios_router
 
 # Crear la aplicación FastAPI
@@ -7,8 +8,9 @@ app = FastAPI()
 # Incluir el router de beneficios sustentables
 app.include_router(beneficios_router)
 
-# Punto de entrada de la aplicación
-if __name__ == "__main__":
-    import uvicorn
-    # Ejecutar la aplicación usando Uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# Crear un manejador de Lambda usando Mangum
+handler = Mangum(app)
+
+# El punto de entrada para AWS Lambda
+def lambda_handler(event, context):
+    return handler(event, context)
